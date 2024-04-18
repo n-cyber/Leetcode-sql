05fdfb91e7a0d9b4fd86985105ce085587252eda
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-04-18 12:58:38 -0700

    Update 1978. Employees Whose Managers left the Company.sql

--- easy/1978. Employees Whose Managers left the Company.sql
+++ easy/1978. Employees Whose Managers left the Company.sql
@@ -1,4 +1,5 @@
 -- Write your PostgreSQL query statement below
+-- Approach 1 - using NOT IN
 SELECT 
   employee_id
 FROM Employees 
@@ -9,4 +10,14 @@ AND manager_id NOT IN
       DISTINCT employee_id 
     FROM Employees
 )
+ORDER BY 1
+
+-- Approach 2 - using LEFT JOIN
+SELECT 
+  e.employee_id
+FROM Employees e LEFT JOIN Employees m
+ON e.manager_id = m.employee_id
+WHERE e.salary < 30000
+AND e.manager_id IS NOT NULL 
+AND m.employee_id IS NULL
 ORDER BY 1
