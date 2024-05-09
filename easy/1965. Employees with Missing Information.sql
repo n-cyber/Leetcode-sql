527565c9abd0411fbe9e082d6135ab7da2b9edfa
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-05-09 12:21:26 -0700

    Create 1965. Employees with Missing Information.sql

---
+++ easy/1965. Employees with Missing Information.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+  e.employee_id
+FROM Employees e
+LEFT JOIN Salaries s ON e.employee_id = s.employee_id
+WHERE s.employee_id IS NULL
+UNION
+SELECT 
+  s.employee_id
+FROM Employees e
+RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
+WHERE e.employee_id IS NULL
+ORDER BY 1
