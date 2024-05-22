5e3b159afee0a67ec622a5d3249a2dd5ac81a837
Nitya <51813914+n-cyber@users.noreply.github.com>
2024-05-21 20:44:50 -0700

    Create 1251. Average Selling Price.sql

---
+++ easy/1251. Average Selling Price.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT
+  p.product_id 
+, COALESCE(ROUND(SUM(p.price*us.units)*1.00/SUM(us.units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold us
+ON p.product_id = us.product_id 
+    AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY 1
